e9b04482028f7ba5b915318206ce1950d1078525
Alexeykaa <ka.alexey@gmail.com>
2023-10-31 14:22:28 +0300

    init implementation

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/FilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/FilteringStrategy.java
@@ -1,5 +1,11 @@
 package com.mjc.stage2;
 
-public interface FilteringStrategy {
+import java.util.function.Predicate;
+
+public interface FilteringStrategy extends Predicate<Product> {
     boolean filter(Product product);
+
+    default boolean test(Product product) {
+        return filter(product);
+    }
 }
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,18 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver  implements Observer {
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle src = event.getSource();
+        RectangleWarehouse store = RectangleWarehouse.getInstance();
+        RectangleValues value = store.get(src.getId());
+        value.setPerimeter(src.getPerimeter());
+        value.setSquare(src.getSquare());
+    }
 }
